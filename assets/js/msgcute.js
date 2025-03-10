8150371aa4192b02976fadffd330fb3925afe972
glrmrissi <guilhermerissi04112006@gmail.com>
2025-03-09 22:06:06 -0300

    style: tiny changes

---
+++ assets/js/msgcute.js
@@ -0,0 +1,3 @@
+// Take ez man
+console.log("%c Olá Dev curioso! 🫡", "font-size: 25px;");
+console.log("%c Considere me chamar para participar de uma entrevista! O contato pode ser via Email!!","font-size: 16px; font-style:bold;");
